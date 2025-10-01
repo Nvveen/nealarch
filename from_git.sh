9cbb9150c6738ee4cabc760c442a4dbab2b31999
Neal van Veen <nealvanveen@gmail.com>
2025-10-01 22:59:38 +0000

    add git to install

--- from_git.sh
+++ from_git.sh
@@ -2,6 +2,7 @@
 
 mkdir -p /tmp/nealhost
 cd /tmp/nealhost || exit
+sudo pacman -Sy git --noconfirm || exit
 git clone https://github.com/Nvveen/nealhost && cd nealhost || exit
 bash install.sh
 rm -rf /tmp/nealhost
