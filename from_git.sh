3e8cff0d7fc095214a2b8a9985cbc2a35775ad1b
Neal van Veen <Nvveen@users.noreply.github.com>
2025-10-02 15:09:13 +0200

    Change repository clone from nealhost to nealarch

--- from_git.sh
+++ from_git.sh
@@ -3,6 +3,6 @@
 mkdir -p /tmp/nealhost
 cd /tmp/nealhost || exit
 sudo pacman -Sy git --noconfirm || exit
-git clone https://github.com/Nvveen/nealhost && cd nealhost || exit
+git clone https://github.com/Nvveen/nealarch && cd nealarch || exit
 bash install.sh
 rm -rf /tmp/nealhost
