6c5646d8888e504e0914b7d2f03e18d87edc243d
Neal van Veen <nealvanveen@gmail.com>
2025-10-16 16:41:19 +0200

    update from_git

--- from_git.sh
+++ from_git.sh
@@ -2,8 +2,7 @@
 
 cd $HOME || exit;
 sudo pacman -Sy git --noconfirm || exit
+mkdir -p $HOME/.local/share
 git clone https://github.com/Nvveen/nealarch
 cd nealarch || exit
 bash install.sh
-cd $HOME || exit;
-rm -rf nealarch
